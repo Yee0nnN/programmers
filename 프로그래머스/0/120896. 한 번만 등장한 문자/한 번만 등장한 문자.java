9b9b735871b1091faeae56084ccd4082a73fb0bb
seyeon <95129568+Yee0nnN@users.noreply.github.com>
2025-05-12 00:15:56 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 2.58 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+	public String solution(String s) {
+		String answer = "";
+		int count = 0;
+		String[] str = s.split("");
+		
+		Arrays.sort(str);
+		for (int i = 0; i < str.length; i++) {
+			count = 0;
+
+			for (int j = 0; j < str.length; j++) {
+				if (str[i].equals(str[j])) {
+					count++;
+				}
+			}
+			if (count == 1) {
+				answer += str[i];
+			}
+		}
+		return answer;
+	}
+}
