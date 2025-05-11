02becc4e8c943297b2e390b9eed35fb293529c08
seyeon <95129568+Yee0nnN@users.noreply.github.com>
2025-05-11 23:55:43 +0900

    편지.java

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,8 +1,6 @@
 class Solution {
 	public int solution(String message) {
-		int answer = 0;
-		int length = message.trim().length();
-		answer = length * 2;
+		int answer = message.trim().length() * 2;
 		return answer;
 	}
 }
