dee2decff5be3edf171b1a4c71a012147b323bec
seyeon <95129568+Yee0nnN@users.noreply.github.com>
2025-05-15 04:42:35 +0900

    [level 0] Title: 특이한 정렬, Time: 1.28 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+	public int[] solution(int[] numlist, int n) {
+		int[] answer = {};
+		Arrays.sort(numlist);
+
+		for (int i = 0; i < numlist.length; i++) {
+			for (int j = 0; j < numlist.length; j++) {
+				if (Math.abs(numlist[i] - n) <= Math.abs(numlist[j] - n)) {
+					int tmp = numlist[i];
+					numlist[i] = numlist[j];
+					numlist[j] = tmp;
+				}
+			}
+		}
+		answer = numlist;
+		return answer;
+	}
+}
