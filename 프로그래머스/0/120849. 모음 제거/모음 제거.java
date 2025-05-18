bc2fdeeb20b4caa40f176b37d96a758cc906d9af
seyeon <95129568+Yee0nnN@users.noreply.github.com>
2025-05-19 03:13:47 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,10 @@
+class Solution {
+	public String solution(String my_string) {
+		String answer = my_string;
+		String[] str = { "a", "e", "i", "o", "u" };
+		for (String s : str) {
+			answer = answer.replace(s, "");
+		}
+		return answer;
+	}
+}
