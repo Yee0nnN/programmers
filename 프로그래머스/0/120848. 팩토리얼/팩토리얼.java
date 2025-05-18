8924968b59ce77c7fa855886d3ab88f9564444a1
seyeon <95129568+Yee0nnN@users.noreply.github.com>
2025-05-19 03:24:45 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 90.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,12 @@
+class Solution {
+	public int solution(int n) {
+		int answer = 1;
+		int fac = 1;
+
+		while (fac * (answer + 1) <= n) {
+			answer++;
+			fac *= answer;
+		}
+		return answer;
+	}
+}
