8ccc8014632336bc212ca5885d7aaf77d5ebc015
seyeon <95129568+Yee0nnN@users.noreply.github.com>
2025-05-18 17:05:56 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,11 @@
+class Solution {
+	public int solution(int a, int b) {
+		int answer = 0;
+		while (b % 2 == 0)
+			b /= 2;
+		while (b % 5 == 0)
+			b /= 5;
+		answer = ((a % b == 0) || (b == 1)) ? 1 : 2;
+		return answer;
+	}
+}
