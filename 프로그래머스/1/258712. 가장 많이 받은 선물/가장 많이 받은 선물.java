6d916153de23c7dc4706dc6a9381c4e1815828bc
seyeon <95129568+Yee0nnN@users.noreply.github.com>
2025-07-20 03:42:04 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 26.06 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,58 @@
+class Solution {
+	public int solution(String[] friends, String[] gifts) {
+		int answer = 0;
+		int[] give = new int[friends.length];
+		int[] get = new int[friends.length];
+
+		int[][] giveList = new int[friends.length][friends.length];
+
+		for (String g : gifts) {
+			String[] giftFriends = g.split(" ");
+			int giver = -1;
+			int getter = -1;
+
+			for (int i = 0; i < friends.length; i++) {
+				if (friends[i].equals(giftFriends[0]))
+					giver = i;
+				if (friends[i].equals(giftFriends[1]))
+					getter = i;
+			}
+
+			giveList[giver][getter]++;
+			give[giver]++;
+			get[getter]++;
+		}
+		
+		// 선물 지수 = 준 선물 - 받은 선물
+        int[] count = new int[friends.length];
+        for (int i = 0; i < friends.length; i++) {
+        	count[i] = give[i] - get[i];
+        }
+        
+		// 다음 달
+		int[] next = new int[friends.length];
+
+		for (int i = 0; i < friends.length; i++) {
+			for (int j = i + 1; j < friends.length; j++) { 
+				if (giveList[i][j] > giveList[j][i]) {
+					next[i]++;
+				} else if (giveList[i][j] < giveList[j][i]) {
+					next[j]++;
+				} else {
+					if (count[i] > count[j]) {
+						next[i]++;
+					} else if (count[i] < count[j]) {
+						next[j]++;
+					}
+				}
+			}
+		}
+
+		int max = 0;
+		for (int k : next) {
+			max = Math.max(max, k);
+		}
+		answer = max;
+		return answer;
+	}
+}
