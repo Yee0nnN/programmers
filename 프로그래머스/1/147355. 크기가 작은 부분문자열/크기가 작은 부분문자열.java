b14d47a9842b97d94369a21a2369a1a5fb8b883b
seyeon <95129568+Yee0nnN@users.noreply.github.com>
2025-07-22 02:46:50 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 8.16 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,6 +1,18 @@
+import java.math.BigInteger;
+
 class Solution {
-    public int solution(String t, String p) {
-        int answer = 0;
-        return answer;
-    }
+	public int solution(String t, String p) {
+		int answer = 0;
+
+		// int 범위 초과
+		BigInteger pNum = new BigInteger(p);
+
+		for (int i = 0; i <= t.length() - p.length(); i++) {
+			BigInteger num = new BigInteger(t.substring(i, i + p.length()));
+			if (num.compareTo(pNum) <= 0) {
+				answer++;
+			}
+		}
+		return answer;
+	}
 }
