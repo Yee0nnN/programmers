cdd1cd00aa2deec24c5905bbcc640ee51833e442
seyeon <95129568+Yee0nnN@users.noreply.github.com>
2025-07-22 02:44:56 +0900

    [level 1] Title: 문자열 나누기, Time: 0.98 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,21 @@
+class Solution {
+	public int solution(String s) {
+		int answer = 0;
+		int count = 0;
+		char x = s.charAt(0);
+
+		for (int i = 0; i < s.length(); i++) {
+			if (count == 0) {
+				x = s.charAt(i);
+				answer++;
+			}
+
+			if (s.charAt(i) == x) {
+				count++;
+			} else {
+				count--;
+			}
+		}
+		return answer;
+	}
+}
